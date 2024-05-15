fd5a5b2898022c1f1c2448350f26d6393926e348
Cata-AF <catiskura@gmail.com>
2024-05-15 12:20:00 -0500

    adiciones base de datos

--- MovilDataCo/adminMDC/models.py
+++ MovilDataCo/adminMDC/models.py
@@ -10,3 +10,78 @@ class Usuarios(models.Model):
     def __str__(self):
         return self.nombre
     
+class Demanda_Abonados(models.Model):
+    ANNO = models.FloatField(default=0)
+    TRIMESTRE = models.FloatField(default=0)
+    MES_DEL_TRIMESTRE = models.FloatField(default=0)
+    ID_EMPRESA = models.TextField(default="")
+    EMPRESA = models.TextField(default="")
+    ID_MODALIDAD_PAGO = models.FloatField(default=0)
+    MODALIDAD_PAGO = models.TextField(default="")
+    ID_TERMINAL = models.FloatField(default=0)
+    TERMINAL = models.TextField(default="")
+    ID_TECNOLOGIA = models.FloatField(default=0)
+    TECNOLOGIA = models.TextField(default="")
+    CANTIDAD_ABONADOS = models.FloatField(default=0)
+
+class Demanda_Ingresos(models.Model):
+    ANNO = models.FloatField(default=0)
+    TRIMESTRE = models.FloatField(default=0)
+    MES_DEL_TRIMESTRE = models.FloatField(default=0)
+    ID_EMPRESA = models.FloatField(default=0)
+    EMPRESA = models.TextField(default="")
+    ID_MODALIDAD_PAGO = models.TextField(default="")
+    MODALIDAD_PAGO = models.TextField(default="")
+    ID_TERMINAL = models.FloatField(default=0)
+    TERMINAL = models.TextField(default="")
+    CANTIDAD_INGRESOS = models.FloatField(default=0)
+
+class Demanda_Trafico(models.Model):
+    ANNO = models.FloatField(default=0)
+    TRIMESTRE = models.FloatField(default=0)
+    MES_DEL_TRIMESTRE = models.FloatField(default=0)
+    ID_EMPRESA = models.FloatField(default=0)
+    EMPRESA = models.TextField(default="")
+    ID_MODALIDAD_PAGO = models.TextField(default="")
+    TRAFICO = models.FloatField(default=0)
+    MODALIDAD_PAGO = models.TextField(default="")
+
+class Cargo_Ingresos(models.Model):
+    ANNO = models.FloatField(default=0)
+    TRIMESTRE = models.FloatField(default=0)
+    MES_DEL_TRIMESTRE = models.FloatField(default=0)
+    ID_EMPRESA = models.FloatField(default=0)
+    EMPRESA = models.TextField(default="")
+    ID_SEGMENTO = models.FloatField(default=0)
+    SEGMENTO = models.TextField(default="")
+    ID_TERMINAL = models.FloatField(default=0)
+    TERMINAL = models.TextField(default="")
+    INGRESOS = models.FloatField(default=0)
+
+class Cargo_Suscriptores(models.Model):
+    ANNO = models.FloatField(default=0)
+    TRIMESTRE = models.FloatField(default=0)
+    MES_DEL_TRIMESTRE = models.FloatField(default=0)
+    ID_SEGMENTO = models.FloatField(default=0)
+    SEGMENTO = models.TextField(default="")
+    ID_EMPRESA = models.FloatField(default=0)
+    EMPRESA = models.TextField(default="")
+    ID_TERMINAL = models.FloatField(default=0)
+    TERMINAL = models.TextField(default="")
+    ID_TECNOLOGIA = models.FloatField(default=0)
+    TECNOLOGIA = models.TextField(default="")
+    CANTIDAD_SUSCRIPTORES = models.FloatField(default=0)
+
+class Cargo_Trafico(models.Model):
+    ANNO = models.FloatField(default=0)
+    TRIMESTRE = models.FloatField(default=0)
+    MES_DEL_TRIMESTRE = models.FloatField(default=0)
+    ID_EMPRESA = models.FloatField(default=0)
+    EMPRESA = models.TextField(default="")
+    CANTIDAD_ABONADOS = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.id
+    
+    class Meta:
+        verbose_name_plural="Demandas"
